6a51b5a10db6c84d0cefea6c018b3a99d1cea080
arun-abraham-87 <arun.abraham@live.com>
2025-04-15 09:13:22 +1000

    Removing Note Editor component

--- frontend/src/components/NotesListing.js
+++ frontend/src/components/NotesListing.js
@@ -15,7 +15,6 @@ const NotesListing = ({ notes, addNote , setNotes, objects ,searchQuery, setSear
     return (
         <div className="rounded-lg border bg-card text-card-foreground shadow-sm max-w-[80%] mx-auto p-6">
             <DateSelectorBar setNoteDate={setNoteDate} />
-            <TextEditor addNotes={addNote} objList={objects} searchQuery={setSearchQuery} />
             <NoteEditor
                 note={{ id: '', content: '' }}
                 text=""

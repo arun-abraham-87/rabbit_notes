d1267f47b62e76a148c847ba0e5f4cb5088a8487
arun-abraham-87 <arun.abraham@live.com>
2025-05-04 14:53:57 +1000

    Remove unused StockPrice and NewsFeed components from TagListing to streamline the codebase and improve maintainability.

--- frontend/src/components/TagListing.js
+++ frontend/src/components/TagListing.js
@@ -11,8 +11,6 @@ import { loadTags, addNewTag, deleteTag, editTag } from '../utils/ApiUtils';
 import { loadNotes } from '../utils/ApiUtils';
 import { addNewNoteCommon } from '../utils/ApiUtils';
 import moment from 'moment';
-import StockPrice from './Stocks.js';
-import NewsFeed from './NewsFeed.js';
 
 const TagListing = () => {
   const [tagSearch, setTagSearch] = useState('');
@@ -358,8 +356,6 @@ const TagListing = () => {
           )}
         </div>
       )}
-      <StockPrice/>
-      <NewsFeed/>
     </div>
   );
 };
